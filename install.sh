13efcf599da7716178383d5872e4bba1cbc40f2c
fernandosalomon <fernandosalomon1992@gmail.com>
2025-05-19 15:40:49 -0300

    fix errors in hyprlock

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #Before executing this script set the HOME_DIR variable to your home directory
 HOME_DIR='/home/ffsalomon'
 
-#ln -s $HOME_DIR/.dotfiles/eww $HOME_DIR/.config/eww
+ln -s $HOME_DIR/.dotfiles/eww $HOME_DIR/.config/eww
 ln -s $HOME_DIR/.dotfiles/hypr $HOME_DIR/.config/hypr
 rm -r $HOME_DIR/.config/kitty
 ln -s $HOME_DIR/.dotfiles/kitty $HOME_DIR/.config/kitty
@@ -14,3 +14,4 @@ ln -s $HOME_DIR/.dotfiles/wallpapers $HOME_DIR/.config/wallpapers
 ln -s $HOME_DIR/.dotfiles/waybar $HOME_DIR/.config/waybar
 ln -s $HOME_DIR/.dotfiles/zsh $HOME_DIR/.config/zsh
 ln -s $HOME_DIR/.dotfiles/.p10k.zsh $HOME_DIR/.p10k.zsh
+
